3484b5243485abee6d3ae9fbf99091fbdba4ff9b
kayrat <kairatomurbek2@gmail.com>
2018-06-13 21:21:52 +0600

    Create social module, create Post models

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-# Register your models here.
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
 
 from accounts.models import User
--- src/main/utils.py
+++ src/main/utils.py
@@ -5,3 +5,15 @@ def avatar_image_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
     return 'avatars/{0}'.format(filename)
+
+
+def post_video_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return 'videos/{0}'.format(filename)
+
+
+def post_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return 'images/{0}'.format(filename)
