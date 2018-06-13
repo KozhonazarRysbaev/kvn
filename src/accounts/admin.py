f2cdfb85b7e444175655c06de595949e553b1c22
kayrat <kairatomurbek2@gmail.com>
2018-06-13 20:25:33 +0600

    Add user for admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'get_full_name')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone', 'sex', 'date_birth', 'avatar', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 from django.contrib.auth.models import BaseUserManager, AbstractUser, \
     PermissionsMixin
 
@@ -38,12 +37,10 @@ class User(AbstractUser):
     Model for storing user, username can be email or facebook id
     """
     phone = models.CharField(max_length=200, verbose_name='Телефон', blank=True, null=True)
-    age = models.PositiveIntegerField(verbose_name='Возраст', blank=True, null=True)
     sex = models.CharField(choices=SEX, max_length=30, verbose_name='Пол', null=True, blank=True)
     avatar = models.ImageField(upload_to=avatar_image_path, verbose_name=u"Изображение")
     date_birth = models.DateField(blank=True, null=True, verbose_name='Дата рождение')
     email = models.EmailField(verbose_name='email address', unique=True)
-
 
     objects = CustomUserManager()
 
