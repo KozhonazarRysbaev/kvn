9edd75f2b6b9f1271456b9589c4616e5f4f52bf1
kayrat <kairatomurbek2@gmail.com>
2018-06-19 23:39:38 +0600

    Вывел пользователей

--- src/social/serializers.py
+++ src/social/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 
+from accounts.models import User
 from social.models import Post
 
 
+class UserPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name')
+
+
 class BasePostSerializer(serializers.ModelSerializer):
+    user = UserPostSerializer(many=False)
+
     class Meta:
         model = Post
-        fields = ('id', 'title', 'description', 'video_file', 'image', 'image_width', 'image_height', 'views')
+        fields = ('id', 'title', 'description', 'video_file', 'image', 'image_width', 'image_height', 'views', 'user')
 
 
 class PostSerializer(serializers.ModelSerializer):
