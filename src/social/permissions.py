589d3b3d896dec88fb2e26fa3def610078c602b0
kairat <kairatomurbek2@gmail.com>
2018-07-25 03:50:36 +0600

    Realized voting for the team

--- src/social/permissions.py
+++ src/social/permissions.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from rest_framework.permissions import BasePermission
 from django.utils.translation import ugettext_lazy as _
-from social.models import Post, Team, RequestDonations
+from social.models import Post, Team, RequestDonations, Voting, Events
 
 
 class IsOwnerSelf(BasePermission):
@@ -26,5 +28,34 @@ class IsOwnerTeam(BasePermission):
     message = 'Not permission'
 
     def has_permission(self, request, view):
-        request_donations = RequestDonations.objects.filter(team=request.user.team_owners.first(), is_active=True).first()
+        request_donations = RequestDonations.objects.filter(team=request.user.team_owners.first(),
+                                                            is_active=True).first()
         return Team.objects.filter(owner=request.user).exists() if not request_donations else False
+
+
+class IsVotedUser(BasePermission):
+    message = _('Вы уже голосовали!')
+
+    def has_permission(self, request, view):
+        if 'event_pk' not in view.kwargs:
+            return False
+
+        try:
+            v = Voting.objects.get(events__pk=view.kwargs['event_pk'], user=request.user)
+            if v:
+                return False
+        except Voting.DoesNotExist:
+            return True
+
+
+class IsVoted(BasePermission):
+    message = _('Устекло время время голосовать!')
+
+    def has_permission(self, request, view):
+        if 'event_pk' not in view.kwargs:
+            return False
+
+        try:
+            return Events.objects.get(pk=view.kwargs['event_pk'], created_at__gt=datetime.now())
+        except Events.DoesNotExist:
+            return False
--- src/social/urls.py
+++ src/social/urls.py
@@ -8,6 +8,7 @@ router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'posts', views.PostVieSet, base_name='posts')
 router.register(r'posts/(?P<post_pk>[0-9]+)/comments', views.PostCommentVieSet, base_name='post_comments')
 router.register(r'events', views.EventVieSet, base_name='events')
+router.register(r'events/(?P<event_pk>[0-9]+)/voting', views.VotingVieSet, base_name='event_voting')
 router.register(r'teams', views.TeamVieSet, base_name='teams')
 router.register(r'donations', views.RequestDonationsVieSet, base_name='donations')
 
